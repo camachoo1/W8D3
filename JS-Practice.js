82339b35866d4d87368332de5af64474bbfc2501
“Omar <ocamacho812@gmail.com>
2023-01-25 14:52:17 -0800

    added functionality to clock and finished absurd bubble sort

--- JS-Practice.js
+++ JS-Practice.js
@@ -1,7 +1,9 @@
 class Clock {
   constructor() {
     // 1. Create a Date object.
+    // this.date = new Date();
     const date = new Date();
+
     // 2. Store the hours, minutes, and seconds.
     this.hours = `${date.getHours()}`.padStart(2, 0);
     this.minutes = `${date.getMinutes()}`.padStart(2, 0);
@@ -9,21 +11,45 @@ class Clock {
     // 3. Call printTime.
     this.printTime();
     // 4. Schedule the tick at 1 second intervals.
+    setInterval(this._tick.bind(this), 1000);
   }
 
   printTime() {
-    // Format the time in HH:MM:SS
+    const formattedTime = [
+      this.hours,
+      this.minutes,
+      this.seconds,
+    ].join(':');
+
     // Use console.log to print it.
-    console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
+    console.log(formattedTime);
   }
 
   _tick() {
     // 1. Increment the time by one second.
     // 2. Call printTime.
-    setInterval(() => {
-      this.seconds += 1;
-      this.printTime();
-    }, 1000);
+    this.incrementSecs();
+    this.printTime();
+  }
+
+  incrementSecs() {
+    this.seconds++;
+    if (this.seconds === 60) {
+      this.seconds = 0;
+      this.incrementMins();
+    }
+  }
+
+  incrementMins() {
+    this.minutes++;
+    if (this.minutes === 60) {
+      this.minutes = 0;
+      this.incrementHrs();
+    }
+  }
+
+  incrementHrs() {
+    this.hours = (this.hours + 1) % 24;
   }
 }
 
@@ -120,6 +146,7 @@ function absurdBubbleSort(arr, sortCompletionCallback) {
   }
 
   // Kick the first outer loop off, starting `madeAnySwaps` as true.
+  outerBubbleSortLoop(true);
 }
 
 absurdBubbleSort([3, 2, 1], function (arr) {
