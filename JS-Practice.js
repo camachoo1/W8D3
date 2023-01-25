d385fd187cbd819aa8744fe070d1c9b69e353573
“Omar <ocamacho812@gmail.com>
2023-01-25 11:57:01 -0800

    added constructor printTime and tick

--- JS-Practice.js
+++ JS-Practice.js
@@ -1,19 +1,29 @@
 class Clock {
   constructor() {
     // 1. Create a Date object.
+    const date = new Date();
     // 2. Store the hours, minutes, and seconds.
+    this.hours = date.getHours();
+    this.minutes = date.getMinutes();
+    this.seconds = date.getSeconds();
     // 3. Call printTime.
+
     // 4. Schedule the tick at 1 second intervals.
   }
 
   printTime() {
     // Format the time in HH:MM:SS
     // Use console.log to print it.
+    console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
   }
 
   _tick() {
     // 1. Increment the time by one second.
     // 2. Call printTime.
+    setInterval(() => {
+      this.seconds += 1;
+      this.printTime();
+    }, 1000);
   }
 }
 
